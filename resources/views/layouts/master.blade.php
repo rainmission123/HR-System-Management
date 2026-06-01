f310f0c6c560648566bf20a0ef4e9c6c71196632
Rian Mission <rianmission6@gmail.com>
2026-06-01 09:20:26 +0800

    favicon changes top

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,13 +2,13 @@
 <html lang="en" class="light scroll-smooth group" data-layout="vertical" data-sidebar="light" data-sidebar-size="lg" data-mode="light" data-topbar="light" data-skin="default" data-navbar="sticky" data-content="fluid" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <title>HR | StarCode - Admin & Dashboard Template</title>
+    <title>HR | System - Admin & Dashboard Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta content="Minimal Admin & Dashboard Template" name="description">
     <meta content="StarCode Kh" name="author">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::to('assets/images/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ URL::to('assets/images/logo-dark.png') }}">
     <!-- Layout config Js -->
     <script src="{{ URL::to('assets/js/layout.js') }}"></script>
     <!-- StarCode CSS -->
